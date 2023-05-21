f04460c77c23d83fbe3e866f35b93fee59a0b688
Alex Cernas <gh70612@gmail.com>
2023-05-21 17:24:20 -0600

    second commit

--- controllers/loginControllers.js
+++ controllers/loginControllers.js
@@ -3,32 +3,42 @@ let nextColorIndex = 0;
 const usernames = [];
 
 const renderLogin = (req, res) => {
-  res.render('login');
+  const alert = req.session.alert;
+  res.render('login', { alert });
 }
 
 const loginUser = (req, res) => {
     const { username } = req.body;
-    const color = colors[nextColorIndex];
-    nextColorIndex = (nextColorIndex + 1) % colors.length;
   
     // verificar si el nombre de usuario ya existe en el array
     if (usernames.includes(username)) {
-        req.session.alert = 'El nombre de usuario ya existe';
+        req.session.alert = 'El usuario ya esta logueado';
         return res.redirect('/login');
       }
-
     // agregar el nombre de usuario al array
     usernames.push(username);
-  
+    const color = colors[nextColorIndex];
+    nextColorIndex = (nextColorIndex + 1) % colors.length;
+    console.log(usernames);   
+      
     req.session.username = username;
     req.session.color = color;
     res.redirect('/chat');
   }
 
-const logoutUser = (req, res) => {
-  req.session.destroy();
-  res.redirect('/login');
-}
+  const logoutUser = (req, res) => {
+    const { username } = req.session;
+    
+    // Eliminar el nombre de usuario del array
+    const index = usernames.indexOf(username);
+    if (index !== -1) {
+      usernames.splice(index, 1);
+    }
+    console.log(usernames); 
+
+    req.session.destroy();
+    res.redirect('/login');
+  };
 
 module.exports = {
   renderLogin,
