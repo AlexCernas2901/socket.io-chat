f7432d582a6d1143698cb5472e25ced3b13ef01d
Alex Cernas <gh70612@gmail.com>
2023-05-22 00:19:09 -0600

    final changes

--- config/socket.js
+++ config/socket.js
@@ -1,20 +1,28 @@
 const userSession = require("./session");
+const sanitizeHtml = require("sanitize-html");
 
 function configureSocket(io) {
   io.use((socket, next) => {
     userSession(socket.request, socket.request.res, next);
   });
 
-  io.on('connection', (socket) => {
+  io.on("connection", (socket) => {
     // evento cuando se recibe un nuevo mensaje del cliente
-    socket.on('chatMessage', (message) => {
+    socket.on("chatMessage", (message) => {
       // obteniendo datos de la sesión
       const { color, username } = socket.request.session;
 
-      io.emit('chatMessage', { message: `[${username}] ${message}`, color, username });
+      // sanitizando el mensaje
+      const sanitizedMessage = sanitizeHtml(message);
+
+      io.emit("chatMessage", {
+        message: `[${username}] ${sanitizedMessage}`,
+        color,
+        username,
+      });
     });
 
-    socket.on('disconnect', () => {
+    socket.on("disconnect", () => {
       const { username } = socket.request.session;
       console.log(`${username}-left`);
     });
